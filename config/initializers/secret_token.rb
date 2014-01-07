946521718d4792f67fd264f1bef5c66a932a1fcf
pampels <gist.jr.pampellona@gmail.com>
2014-01-07 11:41:36 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LyndaSqlite::Application.config.secret_key_base = '3acd2099c51be49aa1fb0be4bb71c42314cf35fc74ced3d957012445c9a7405b76888ecb94e6dd0bf822a71961c4932197ccbcbbc2e48ab0ad37f08389a5c61e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LyndaSqlite::Application.config.session_store :cookie_store, key: '_lynda_sqlite_session'
