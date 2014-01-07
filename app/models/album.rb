5ee06fc7eafa616e51ef24c609eaa42e51b12e8e
pampels <gist.jr.pampellona@gmail.com>
2014-01-07 16:55:14 +0800

    added database relationship between albums and artists

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,4 @@
 class Album < ActiveRecord::Base
+	belongs_to :artist
 	validates :title, :artist, :description, :published_date, :genre, :artist_id, :presence => true
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,4 @@
 class Artist < ActiveRecord::Base
+	has_many :albums
 	validates :name, :biography, :birthdate, :presence => true
 end
