d5b5a4f7a791c7077017e1491b400fb8701a9db1
pampels <gist.jr.pampellona@gmail.com>
2014-01-08 23:26:33 +0800

    added customers validation

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -69,6 +69,6 @@ class CustomersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def customer_params
-      params.require(:customer).permit(:name, :lastname)
+      params.require(:customer).permit(:name, :lastname, :email)
     end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,5 @@
 class Customer < ActiveRecord::Base
 	has_many :orders
+	validates :name, :lastname, :email, presence: true
+	validates :email, uniqueness: true
 end
