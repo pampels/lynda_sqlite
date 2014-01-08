b29344ef999e92a44e15e8e846c453c571509b55
pampels <gist.jr.pampellona@gmail.com>
2014-01-08 22:44:10 +0800

    added relationships to line_items, albums, and orders

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,6 @@
 class Album < ActiveRecord::Base
 	belongs_to :artist
+	has_many :line_items
 	has_many :features_assignments
 	has_many :features, through: :features_assignments
 	validates :title, :description, :published_date, :genre, :artist_id, :presence => true
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,2 +1,4 @@
 class LineItem < ActiveRecord::Base
+	belongs_to :album
+	belongs_to :order
 end
