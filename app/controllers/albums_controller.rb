596f1057f2a60df002d2177bcd89f1c1812c34d7
pampels-obimapp-mac <gist.jr.pampellona@gmail.com>
2014-01-09 17:19:23 +0800

    modified album covers

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -73,7 +73,7 @@ class AlbumsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def album_params
-      params.require(:album).permit(:title, :description, :published_date, :genre, :artist_id, :price)
+      params.require(:album).permit(:title, :description, :published_date, :genre, :artist_id, :price, :image_path)
     end
 
     def add_album_features
