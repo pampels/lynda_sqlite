12cf077c2f33d2163a1e612b2c4a1cf9ad0fafe3
pampels-obimapp-mac <gist.jr.pampellona@gmail.com>
2014-01-09 15:37:33 +0800

    modified albums to show it's price

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,5 +3,5 @@ class Album < ActiveRecord::Base
 	has_many :line_items
 	has_many :features_assignments
 	has_many :features, through: :features_assignments
-	validates :title, :description, :published_date, :genre, :artist_id, :presence => true
+	validates :title, :description, :published_date, :genre, :artist_id, :price, :presence => true
 end
