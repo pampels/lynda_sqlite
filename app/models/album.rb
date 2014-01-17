2250116646c8e68a137d780fa42eae44e761b43d
pampels-obimapp-mac <gist.jr.pampellona@gmail.com>
2014-01-17 14:42:55 +0800

    added a relationship between album, cart, and line_item and made sure an album is not referenced by any line_item before it is destroyed

--- app/models/album.rb
+++ app/models/album.rb
@@ -4,4 +4,17 @@ class Album < ActiveRecord::Base
 	has_many :features_assignments
 	has_many :features, through: :features_assignments
 	validates :title, :description, :published_date, :genre, :artist_id, :price, :image_path, :presence => true
+
+	before_destroy :ensure_not_referenced_by_any_line_item
+
+	# ensure that there are no line items referencing this product
+	def ensure_not_referenced_by_any_line_item
+		if line_items.count.zero?
+			return true
+		else
+			errors.add(:base, 'Cannot delete. This product is currently in a line item')
+			return false
+		end
+
+	end
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,5 @@
 class LineItem < ActiveRecord::Base
 	belongs_to :album
 	belongs_to :order
+	belongs_to :cart
 end
