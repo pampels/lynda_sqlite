f3ae494cf81c4a0030a514e68c6b464f2dfdd19f
pampels-obimapp-mac <gist.jr.pampellona@gmail.com>
2014-01-17 14:12:38 +0800

    migrated create_carts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140109083915) do
+ActiveRecord::Schema.define(version: 20140117060952) do
 
   create_table "albums", force: true do |t|
     t.string   "title"
@@ -29,6 +29,11 @@ ActiveRecord::Schema.define(version: 20140109083915) do
     t.string   "name"
     t.text     "biography"
     t.date     "birthdate"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "carts", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
