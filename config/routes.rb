dfcf5ec081230887c02e4d27bf14f6454865807f
pampels-obimapp-mac <gist.jr.pampellona@gmail.com>
2014-01-17 15:19:43 +0800

    added line_items to routes and created 'Add to Cart' button

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ LyndaSqlite::Application.routes.draw do
 
   resources :albums
 
+  resources :line_items
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
